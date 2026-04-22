3302aa5d8700bd3cc05f53fd0a7e4cbaff9ef5ef
Deividy Terra <deividyterra2@gmail.com>
2026-04-22 17:32:07 -0300

    Entendendo Variáveis

---
+++ 3 - Variáveis/app1.js
@@ -0,0 +1,17 @@
+// variáveis em js
+
+/*
+VAR = Maneira antiga
+LET = Adiciona valor que pode ser alterado
+CONST = Adiciona valor e posteriormente nao podemos usar
+*/
+
+var nome = "Deividy";
+console.log(`Meu nome é: ${nome}`);
+
+let nomeLet = "Deividy T";
+nomeLet = "Terra";
+console.log(`Meu nome é: ${nomeLet}`);
+
+const nomeConst = "Deividy T";
+console.log(`Meu nome é: ${nomeConst}`);
