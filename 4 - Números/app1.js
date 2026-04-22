0f813042db1cbcaeb423ada091082f38bb13ed06
Deividy Terra <deividyterra2@gmail.com>
2026-04-22 17:40:41 -0300

    Um pouco mais sobre números

---
+++ 4 - Números/app1.js
@@ -0,0 +1,10 @@
+// float, double, integers = 
+
+console.log(10 + 11.3333333);
+
+// casas decimais são separadas por .
+console.log(12, 10 + 3, 3);
+
+const idade = 31;
+console.log(idade);
+console.log(idade + 2);
