d34a641396b8b9a6544b831e068f44e4cb32cbe0
Deividy Terra <deividyterra2@gmail.com>
2026-04-22 18:32:30 -0300

    Operadores de Comparação

---
+++ 7 - Operadores de Comparação/app1.js
@@ -0,0 +1,47 @@
+// Igual
+console.log("10 == 10", 10 == "10");
+console.log("11 == 10", 11 == 10);
+console.log("ab == aa", "ab" == "aa");
+
+console.log("===============");
+
+// Diferente
+console.log("10 != 10", 10 != 10);
+console.log("11 != 10", 11 != 10);
+console.log("ab != aa", "ab" != "aa");
+
+console.log("===============");
+
+// Estritamente Igual: compara o conteudo e o tipo de dado
+console.log("10 === 10", 10 === 10);
+console.log("11 === 11", "11" === 11);
+
+console.log("===============");
+
+// Estritamente Diferente: compara o conteudo e tipo de dado
+console.log("10 !== 10", 10 !== 10);
+console.log("11 !== 11", "11" !== 11);
+
+console.log("===============");
+
+// Maior
+console.log("10 > 10", 10 > 10);
+console.log("11 > 10", 11 > 10);
+
+console.log("===============");
+
+// Menor
+console.log("10 < 10", 10 < 10);
+console.log("11 < 10", 11 < 10);
+
+console.log("===============");
+
+// Maior Igual
+console.log("10 >= 10", 10 >= 10);
+console.log("11 >= 10", 11 >= 10);
+
+console.log("===============");
+
+// Menor Igual
+console.log("10 <= 10", 10 <= 10);
+console.log("11 <= 10", 11 <= 10);
