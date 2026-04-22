29cfd9b36645a372270ed8a39058df1f10ba4439
Deividy Terra <deividyterra2@gmail.com>
2026-04-22 18:02:45 -0300

    Operadores de Atribuição

---
+++ 6 - Operadores de Atribuição/app1.js
@@ -0,0 +1,17 @@
+// Atribuição comum
+let idade;
+idade = 30;
+console.log(idade);
+
+// Atribuição de adição
+let caixa = 1000;
+caixa = caixa + 10;
+console.log(`Ás 10h da manhã o saldo era de ${caixa}`);
+
+caixa += 500;
+
+console.log(`Á 18h, o saldo era de ${caixa}`);
+
+caixa -= 200;
+
+console.log(`Após pagar as contas de luz o saldo ficou de ${caixa}`);
