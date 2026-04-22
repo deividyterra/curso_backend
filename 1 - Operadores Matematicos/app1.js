1b69d013e244fc3e2759c259bcc21b8e08bace6d
Deividy Terra <deividyterra2@gmail.com>
2026-04-22 17:09:23 -0300

    Operações Básicas

---
+++ 1 - Operadores Matematicos/app1.js
@@ -0,0 +1,20 @@
+// comentarios
+
+/* comentarios
+    de linhas
+
+*/
+
+console.log("O 'console.log' exibe um valor no console do terminal"); 
+
+//somar 
+console.log("A soma é: ", 10 + 1);
+
+//subtrair
+console.log("A subtracao é: ", 10 - 1);
+
+//dividir
+console.log("A divisão é: ", 10 / 3);
+
+//multiplicar
+console.log("A multiplicacao é: ", 10 * 3);
