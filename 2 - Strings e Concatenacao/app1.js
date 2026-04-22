93eca72cf0b18c3e8106aa494b39bfadadbcbb93
Deividy Terra <deividyterra2@gmail.com>
2026-04-22 17:17:34 -0300

    Strings e Concatenacao

---
+++ 2 - Strings e Concatenacao/app1.js
@@ -0,0 +1,10 @@
+// Strings são letras, números e caracteres diversos. Textos e palavras
+console.log("Deividy Terra");
+
+// Concatenacao
+console.log("A minha idade é: " + 20);
+
+// Concatenacao com template string ou template literals
+console.log(`A minha idade é ${31}`);
+
+console.log(`Essa alternativa é: ${false}`);
