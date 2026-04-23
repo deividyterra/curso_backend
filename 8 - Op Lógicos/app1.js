ce7e108a4cc8395fbb0b32a0a59fd6fbc83a8e9f
Deividy Terra <deividyterra2@gmail.com>
2026-04-23 17:34:29 -0300

    Operadores Lógicos

---
+++ 8 - Op Lógicos/app1.js
@@ -0,0 +1,17 @@
+ // Operadores Lógicos
+
+ // and (&&)
+
+ console.log("10 >= 10 && 11 >= 11", 10 >= 10 && 11 >= 11);
+
+ // or (||)
+
+ console.log("10 >= 10 || 11 >= 11", 10 >= 10 || 11 >= 11);
+
+ // not (!)
+
+ const usuarioAtivo = false;
+ const inscrito = true;
+
+ console.log("not", !usuarioAtivo);
+ console.log("not", !inscrito);
