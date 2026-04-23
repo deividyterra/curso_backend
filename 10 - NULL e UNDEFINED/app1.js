edaef86eeae5e0d272e211608b7d7c8e17403ca2
Deividy Terra <deividyterra2@gmail.com>
2026-04-23 17:56:28 -0300

    NULL e UNDEFINED

---
+++ 10 - NULL e UNDEFINED/app1.js
@@ -0,0 +1,13 @@
+//NULL
+
+let meuNome = null;
+console.log(meuNome);
+meuNome = "Daniel";
+console.log(meuNome);
+
+//UNSIGNED
+
+let meuSobrenome;
+console.log(meuSobrenome);
+meuSobrenome = "Lopes";
+console.log(meuSobrenome);
---
+++ 9 - Convenções/app1.js
@@ -0,0 +1,2 @@
+const nome = "Daniel";
+// Muitas palavras são reservadas da linguagem, portanto devem ser aprendidas com o tempo
